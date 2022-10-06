4730259cf4e1bc002d887c14158137bbb6d95a11
shanzventures <shanmcgov93@gmail.com>
2022-10-06 11:24:11 +0100

    Imported all code

---
+++ Final-Weather-Project/Weather-App-Project/src/index.js
@@ -0,0 +1,77 @@
+// Date / Time
+
+let h1 = document.querySelector("h1");
+
+let now = new Date();
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let month = months[now.getMonth()];
+
+h1.innerHTML = `${day} ${date} ${month}, ${hours}:${minutes}, ${year}`;
+
+///////// City Search
+
+function displayWeatherCondition(response) {
+  document.querySelector(".city-input").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let apiKey = "8e9b90927c2534dbc9ab90cce8c00178";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${city}`;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+/////////////////// Location button
+
+function searchLocation(position) {
+  let apiKey = "8e9b90927c2534dbc9ab90cce8c00178";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
